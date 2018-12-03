f091b51ab871d968771593f126fc0b14aa7892f4
Moren YANG <morenyang88@gmail.com>
2018-12-03 22:54:31 +0800

    add issue components

--- src/App.js
+++ src/App.js
@@ -1,25 +1,19 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import { Input, Select } from 'antd';
+import './app.less';
+
+const { Option } = Select;
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+      <div>
+        <div style={{ width: 200, margin: 32 }}>
+          <Input />
+          <Select value="Hello">
+            <Option value="Hello">Hello</Option>
+          </Select>
+        </div>
       </div>
     );
   }
